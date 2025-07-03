78600b030ef40a7e104327595f2477ea5a40069d
Bob Leung <bobleung@Bobs-MacBook-Pro.local>
2025-07-03 13:22:56 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import RubyPlugin from 'vite-plugin-ruby'
+
+export default defineConfig({
+  plugins: [
+    svelte(),
+    tailwindcss(),
+    RubyPlugin(),
+  ],
+  server: {
+    host: 'localhost',
+    strictPort: true,
+    hmr: {
+      host: 'localhost'
+    }
+  }
+})
