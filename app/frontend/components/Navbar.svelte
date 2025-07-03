a1bf5582eb439999125a15c441ae980365ef615c
Bob Leung <bobleung@Bobs-MacBook-Pro.local>
2025-07-03 15:59:20 +0200

    feat: add responsive navigation bar with user dropdown menu

---
+++ app/frontend/components/Navbar.svelte
@@ -0,0 +1,48 @@
+<script>
+  export let userFirstName = "John";
+</script>
+
+<div class="navbar bg-base-100 shadow-sm">
+  <div class="navbar-start">
+    <div class="dropdown">
+      <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
+        </svg>
+      </div>
+      <ul
+        tabindex="0"
+        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li><a href="/bookings">Bookings</a></li>
+        <li><a href="/invoices">Invoices</a></li>
+        <li class="border-t border-base-300 mt-2 pt-2">
+          <a href="/profile">Profile</a>
+        </li>
+        <li><a href="/logout">Log Out</a></li>
+      </ul>
+    </div>
+    <a class="btn btn-ghost text-xl" href="/">myLocums</a>
+  </div>
+  <div class="navbar-center hidden lg:flex">
+    <ul class="menu menu-horizontal px-1">
+      <li><a href="/bookings">Bookings</a></li>
+      <li><a href="/invoices">Invoices</a></li>
+    </ul>
+  </div>
+  <div class="navbar-end">
+    <div class="dropdown dropdown-end">
+      <div tabindex="0" role="button" class="btn btn-ghost">
+        {userFirstName}
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+        </svg>
+      </div>
+      <ul
+        tabindex="0"
+        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-32 p-2 shadow">
+        <li><a href="/profile">Profile</a></li>
+        <li><a href="/logout">Log Out</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
